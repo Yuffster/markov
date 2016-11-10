eab773be50d52b02b8767bde4a749ed5120c2929
Michelle Steigerwalt <msteigerwalt@gmail.com>
2016-11-10 15:53:50 -0500

    Yeah, I'm not uploading my corpus to GitHub.  Sorry.

--- scratch.py
+++ scratch.py
@@ -1,6 +1,6 @@
 from markov import MarkovChain
 
 m = MarkovChain(size=3)
-m.integrate(open('corpus.txt').read())
+m.integrate(open('ignore/corpus.txt').read())
 print('Vocab length', len(m._chains.keys()))
 print(" ".join(list(m.generate(100))))
