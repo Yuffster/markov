c11c970761f709005391edb32f187dd1d42425cb
Michelle Steigerwalt <msteigerwalt@gmail.com>
2016-11-10 17:59:49 -0500

    Experimenting with overlaps.
    Super not very performant on the size of this corpus.

--- markov.py
+++ markov.py
@@ -9,13 +9,16 @@ class MarkovChain():
 
     _chains = None  # {}
     _size = 1
+    _key_shuffle = None  # Shuffle list of keys in chain.
+    _overlap = None
 
     def __init__(self, size=1):
         self._size = size
         self._chains = {}
+        self._key_shuffle = []
 
     def _get_words(self, text):
-        for w in re.finditer(r"[A-Za-z0-9!\-':\)\(\"?,\.]+", text):
+        for w in re.finditer(r"[A-Za-z!\-'\)\(\"?,\.]+", text):
             yield w.group(0)
 
     def _get_groups(self, text):
@@ -29,6 +32,9 @@ class MarkovChain():
             except StopIteration:
                 break
 
+    def _shuffle(self):
+        random.shuffle(self._key_shuffle)
+
     def _select_next(self, key):
         probs = 0
         p_range = {}
@@ -39,6 +45,13 @@ class MarkovChain():
         for p, node in sorted(p_range.items()):
             if p >= v:
                 return node
+
+    def _get_overlapping_node(self, node, overlap):
+        self._shuffle()
+        node = node[overlap*-1:]
+        for k in self._key_shuffle:
+            if k[overlap*-1:] == node:
+                return k
 
     def _normalize_text(self, text):
         text = text.replace("“", '"')  # Get rid of “smart” qutoes.
@@ -55,11 +68,20 @@ class MarkovChain():
                 self._chains.setdefault(prev, {})
                 self._chains[prev][group] = self._chains[prev].get(group, 0) + 1
             prev = group
-    
-    def generate(self, length=100, start=None):
+        self._key_shuffle = list(self._chains.keys())
+        self._shuffle()
+
+    def _get_random_node(self):
+        self._shuffle()
         for k in self._chains.keys():
-            prev = k
-            break
+            return k
+
+    def generate(self, length=100, start=None, overlap=None):
+        prev = self._get_random_node()
         for _ in range(length):
+            if overlap:
+                prev = self._get_overlapping_node(prev, overlap)
+                if prev is None:
+                    prev = self._get_random_node()
             prev = self._select_next(prev)
             yield " ".join(prev).replace('NEWLINE', "")
--- scratch.py
+++ scratch.py
@@ -3,4 +3,5 @@ from markov import MarkovChain
 m = MarkovChain(size=3)
 m.integrate(open('ignore/corpus.txt').read())
 print('Vocab length', len(m._chains.keys()))
-print(" ".join(list(m.generate(100))))
+print(" ".join(list(m.generate(100, overlap=2))))
+
